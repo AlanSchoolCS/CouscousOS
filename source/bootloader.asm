ee1d50605a640140fb9986ad9d5001f56f729cb7
cuitin <s0083431@bis-school.com>
2024-05-28 22:51:42 +0200

    [Bootloader] Changed to basic string printing system

--- source/bootloader.asm
+++ source/bootloader.asm
@@ -1,38 +1,43 @@
 BITS 16
-org 0x7C00
+org 0x7C00      ; start address
 
 start:
     mov ax, 0x0003      ; Text mode
     int 0x10            ; Interrupt which calls BIOS video services
 
-    ; Print "Hello, World!"
-    mov si, hello_msg   ; Move address of the message into si
+    mov si, os_title
+    call print_string
 
-print_chars:            ; print string of characters
+    mov si, thanks
+    call print_string
+
+    jmp hang
+
+
+print_string:           ; print string of characters
     lodsb               ; Load next character into al
     or al, al           ; Check if the char is the end of the string (zero)
-    jz wait_for_key     ; if zero, it's the end of the string, so we can wait for the next character
+    jz .end             ; end on null terminated string
     mov ah, 0x0E        ; BIOS teletype function for character printing
     int 0x10            ; call BIOS print
-    jmp print_chars     ; loop (repeat for next character input)
-
-wait_for_key:
-    ; infinite loop to allow characters to be entered
-    call read_character
-    call display_character
-    jmp wait_for_key
-
-read_character:
-    mov ah, 0
-    int 0x16            ; get next character using BIOS keyboard service
-    ret                 ; go back to "wait_for_key"
+    jmp print_string
+.end:
+    ret
 
 display_character:      ; print a single character in al
     mov ah, 0x0E        ; BIOS teletype function
     int 0x10            ; Print character in AL
-    ret                 ; go back to "wait_for_key"
+    ret
 
-hello_msg db 'Hello, World! :)', 0
+hang:
+    cli
+    hlt
+    jmp hang
 
-times 510-($-$$) db 0
-dw 0xAA55
+; strings:
+os_title db 'CouscousOS Alpha Testing', 0
+thanks db 13, 10, 13, 10, 'Thank you for using Couscous! :)', 0
+
+
+times 510-($-$$) db 0   ; Make total bootloader size equal to 510 bytes
+dw 0xAA55               ; Boot signature
